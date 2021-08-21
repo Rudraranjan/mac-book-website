d53acb8803bf1b6947967ea1319baef207fd53e6
Rudraranjan Thakur <rudraranjan.thakur@gmail.com>
2021-08-21 15:09:03 +0530

    full complete assignment

--- js/app.js
+++ js/app.js
@@ -105,11 +105,41 @@ function calculationAmount(){
     const extraStorage = amountInput('extra-storage');
     const deliveryCharge = amountInput('delivery-charge');
     const totalPrice = amountInput('total-price');
+    const totalBottomPrice = amountInput('bottom-total');
 
     var totalAmountCal = extraMemory + extraStorage + deliveryCharge + basePriceAmount;
 
-    document.getElementById('total-price').innerText = totalAmountCal;        
+    document.getElementById('total-price').innerText = totalAmountCal; 
+    document.getElementById('bottom-total').innerText = totalAmountCal;       
 } 
+
+// coupon discount code here
+
+document.getElementById('apply-button').addEventListener('click', function(){
+
+    // get input value here
+
+    const inputElement = document.getElementById('input-apply');
+    const inputInnerValue = inputElement.value;
+
+    //maching with coupon code here
+
+    if(inputInnerValue == 'stevekaku'){
+        //total price id getting
+        const bottomtotal = document.getElementById('total-price');
+        // bottom price id getting
+        
+        const bottomtotalMainId = document.getElementById('bottom-total');
+       
+        const bottomTotalParse = parseInt(bottomtotal.innerText);
+        const couponDiscount =  bottomTotalParse *20/100;
+        const couponDiscountFinal = bottomTotalParse - couponDiscount;
+        bottomtotalMainId.innerText= couponDiscountFinal;
+
+    }
+
+})
+
 
 
 
